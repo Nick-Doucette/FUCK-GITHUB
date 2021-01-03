c1e1f0349fd88e9da71279ce99bef5613fdf3da1
Benjamin08 <RFLXcolby@gmail.com>
2021-01-03 03:40:39 -0400

    Multiple Fights
    3, 4 and 5 sided bosses can fight now

---
+++ Assets/Colbys Scripts/GameLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLogic : MonoBehaviour
+{
+
+    public int Round;
+    public Boss boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        boss = GameObject.FindGameObjectWithTag("Boss").GetComponent<Boss>();
+        //Round = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(boss.GetIsDead())
+        {
+            switch(Round)
+            {
+                case 1:
+                    SceneManager.LoadScene("Colbys Second Scene");
+                    break;
+            }
+            
+        }
+    }
+}
